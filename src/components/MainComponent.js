f2e42cdac91131362f3a1f477ca0f7012c8f89df
Vidyansh31 <deva5084@gmail.com>
2021-09-09 19:28:40 +0530

    Fetch from Server

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,14 +13,16 @@ import About from './AboutComponent';
 import {Switch,Route,Redirect,withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 
-import {addComment,fetchDishes} from '../redux/ActionCreators';
+import {addComment,fetchDishes,fetchComments,fetchPromos} from '../redux/ActionCreators';
 
 import {actions} from 'react-redux-form';
 
 const mapDispatchToProps = (dispatch) => ({
   addComment : (dishId,rating,author,comment) => dispatch(addComment(dishId,rating,author,comment)),
   fetchDishes :() => dispatch(fetchDishes()),
-  resetFeedbackForm : () => {dispatch(actions.reset('feedback'))}
+  resetFeedbackForm : () => {dispatch(actions.reset('feedback'))},
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos())
 })
 
 const mapStateToProps = (state) =>{
@@ -40,6 +42,8 @@ class Main extends Component {
 
   componentDidMount() {
     this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
   }
 
   render() {
@@ -48,7 +52,9 @@ class Main extends Component {
         <Home  dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
               dishesLoading={this.props.dishes.isLoading}
               dishesErrMess={this.props.dishes.errmess}
-              promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+              promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+              promoLoading={this.props.promotions.isLoading}
+              promoErrMess={this.props.promotions.errmess}
               leader={this.props.leaders.filter((leader) => leader.featured)[0]}
               
         />
@@ -61,7 +67,9 @@ class Main extends Component {
           <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
           isLoading={this.props.dishes.isLoading}
           ErrMess={this.props.dishes.errmess}
-          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} addComment={this.props.addComment}/>
+          comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
+          addComment={this.props.addComment}
+          commentsErrMess={this.props.comments.errmess}/>
       );
     };
 
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Card, CardImg, CardTitle, CardImgOverlay,Breadcrumb,BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseURL';
 
 
 //Adding Functional component
@@ -10,7 +11,7 @@ function RenderMenuItems({ dish }) {
     return (
         <Card >
             <Link to={`/menu/${dish.id}`}>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
                 </CardImgOverlay>
